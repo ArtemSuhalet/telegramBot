f095761e02e2899b192450b3db0e2efbbdc2010f
Artsiom Sukhalet <2578947@mail.ru>
2022-09-23 18:55:24 +0300

    add1

--- config_data/config.py
+++ config_data/config.py
@@ -6,9 +6,9 @@ if not find_dotenv():
 else:
     load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv('BOT_TOKEN')#5647023944:AAFycsOng1PsagKmGHWB4cDzruIkIcfb0es
 
-RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+RAPID_API_KEY = os.getenv('RAPID_API_KEY')#Artem_suhalet23
 DEFAULT_COMMANDS = (
     ('start', "Запустить бота"),
     ('help', "Вывести справку"),
