52612601e3c8881989d3578fc2167e2a15cd35c1
Artsiom Sukhalet <2578947@mail.ru>
2022-09-23 21:43:30 +0300

    1

--- handlers/default_heandlers/lowprice.py
+++ handlers/default_heandlers/lowprice.py
@@ -4,6 +4,6 @@ from loader import bot
 
 @bot.message_handler(commands=['lowprice'])
 def bot_lowprice(message: Message):
-    msg = bot.reply_to(message, message.from_user.id, 'Введите город, где будет проводится поиск.')
+    msg = bot.reply_to(message.from_user.id, 'Введите город, где будет проводится поиск.')
     bot.register_next_step_handler(msg, data.find_location)#функция по поиску отеля пока не создана
 
