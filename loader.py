6569c4065937d850eef8f5b434d54306a73a1c60
Artsiom Sukhalet <2578947@mail.ru>
2022-10-04 17:51:57 +0300

    commit

--- loader.py
+++ loader.py
@@ -1,7 +1,7 @@
 import pymysql
 from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
-from config_data import config, host, user, password, db_name
+from config_data import config
 from peewee import *
 
 
@@ -9,10 +9,4 @@ from peewee import *
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
 
-my_db = MySQLDatabase(
-    database=db_name,
-    user=user,
-    password=password,
-    host=host,
-    port=3306
-)
+my_db = pymysql.connect(database=db_name, user=user, password=password, host=host, port=3306)
